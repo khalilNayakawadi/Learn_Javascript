c2772704d9205c9ae2cea6c9a5a1f783746a96a7
khalilNayakawadi <136471783+khalilNayakawadi@users.noreply.github.com>
2024-10-01 11:13:18 +0530

    LeapYear Program

---
+++ Programs/LeapYear.js
@@ -0,0 +1,22 @@
+// 1.Dvisible by 4 2.Should not devisible by 100 3.Divisible by 400 = leap year
+
+class LeapYear {
+    year;
+
+    findLeapYear(year) {
+        this.year  = year;
+        if((this.year % 4 == 0 && this.year % 400 == 0) || this.year % 100 != 0){
+            console.log(year, 'is leap year');
+            
+        } else{
+            console.log(year, 'is not a leap year');
+            
+        }
+    }
+
+}
+
+
+let checkLeapYear = new LeapYear;
+checkLeapYear.findLeapYear(2000);
+
