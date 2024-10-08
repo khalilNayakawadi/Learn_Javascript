7eb3371bfe62143f90cd4926a326285d713b62db
Khalil Nayakawadi <136471783+khalilNayakawadi@users.noreply.github.com>
2024-10-08 19:57:10 +0530

    started with JQuery

---
+++ JQuery/script.js
@@ -0,0 +1,16 @@
+//adding js file add testing it with the alert
+alert("Welcome to JQuery Tutorial");
+
+// selecting element just like document.querySelector() in js  
+$("h2").css("color","RoyalBlue")
+
+// adding class to element using Jquery just like a addClass() in js
+$("h2").addClass("heading");
+
+//cheking perticular element having perticular class associated with it or not
+console.log($("h2").hasClass("heading")); // true;
+
+// manipulating text of perticular element
+$("h2").text(" JQuery Tutorial by Khalil")
+
+
